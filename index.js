3181ac5edd48a0149f1da3d2929ef4f7fd32e187
iliaskap12-laptop <iliaskapareliotis@gmail.com>
2020-11-28 19:01:13 +0200

    Finishing initial setup

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
 
 const http = require('http');
 const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
 
 // The server should respond to all requests with a string
 const server = http.createServer((req, res) => {
@@ -17,11 +18,32 @@ const server = http.createServer((req, res) => {
     const path = parsedUrl.pathname;
     const trimmedPath = path.replace(/^\/+|\/+$/g, '');
 
-    // Send the response
-    res.end('Hello World\n');
+    // Get the query string as an object
+    const queryStringObject = parsedUrl.query;
 
-    // Log the request path
-    console.log('Request is received on path: ' + trimmedPath);
+    // Get the HTTP method
+    const httpMethod = req.method.toUpperCase();
+
+    // Get the headers as an object
+    const headers = req.headers;
+
+    // Get the payload, if any
+    const decoder = new StringDecoder('utf-8');
+    let buffer = '';
+
+    req.on('data', (data) => {
+        buffer += decoder.write(data);
+    });
+
+    req.on('end', () => {
+        buffer += decoder.end();
+
+        // Send the response
+        res.end('Hello World\n');
+
+        // Log the request path
+        console.log('Payload: ', buffer);
+    });
 })
 
 
